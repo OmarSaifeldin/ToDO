d15778f84fcca7f42884b3814d9124f9536d3556
Omar Saifeldin <omarsaifeldin252@@gmail.com>
2023-08-07 10:32:29 +0400

    Initial Commit

---
+++ ToDO/ToDOApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDOApp.swift
+//  ToDO
+//
+//  Created by Omar Saifeldin on 07/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
